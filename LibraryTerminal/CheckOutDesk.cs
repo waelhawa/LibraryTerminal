7290ac6ccc3d431508591e54368974af997eee36
waelhawa <wael.hawa86@gmail.com>
2021-05-20 21:52:49 -0400

    structure created

--- LibraryTerminal/CheckOutDesk.cs
+++ LibraryTerminal/CheckOutDesk.cs
@@ -6,5 +6,9 @@ namespace LibraryTerminal
 {
     class CheckOutDesk
     {
+        //VerifyCheckout(Media)
+        //Checkout(Media)
+
+        //Return() - check if dueDate is after today (DateTime .GreaterThan (dueDate, DateTime.Today), change boolCheckout to false
     }
 }
